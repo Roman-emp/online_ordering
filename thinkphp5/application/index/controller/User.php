7a9db7ecc4b0751b0e330c4b56f482cd298b6e66
yar <549137663@qq.com>
2017-07-19 12:01:17 +0800

    登录

--- thinkphp5/application/index/controller/User.php
+++ thinkphp5/application/index/controller/User.php
@@ -1,40 +1,31 @@
 <?php
-namespace app\index\controller;
-use think\Controller;
-//解决重名问题，是用as
-use app\index\model\User as UserModel;
 
-class User extends Controller
-{
-	protected $user;
-	public function _initialize()
-	{
-		$this->user = new UserModel();
-	}
+	namespace app\index\controller;
+	use think\Controller;
+	use app\index\model\User as UserModel;
 
-	public function listUser()
+	class User extends Controller
 	{
-		$data = $this->user->getUserInfo();
-		$this->assign('data',$data);
-		// return $this->fetch();
-	}
-	public function login()
-	{
-		return "登录啊";
-	}
+		/*protected $user;
+		public function _initialize()
+		{
+			$this->user = new UserModel();
+		}*/
+		public function user()
+		{
+				//getUserInfo();返回数据库表的类型、字段等
+			//$data = $this->user->getUserInfo();
+			/*$data = $this->user->test();
+			dump($data);*/
+			
+		}
+		public function register()
+		{
+			return $this->fetch();
+		}
+		public function login()
+		{	
+			return $this->fetch();
+		}
 
-	public function multi()
-	{
-		$data = $this->user->multiTable();
-		var_dump($data);
-		die;
 	}
-	public function test()
-	{
-		//1 find和select的区别
-		//find只取第一条数据，返回一维数组 如果没有结果返回null
-		//select 返回多条数据，返回二维数组，如果没有结果返回一个
-		//空数组
-		var_dump($this->user->countUser());
-	}
-}
--- thinkphp5/application/index/model/User.php
+++ thinkphp5/application/index/model/User.php
@@ -1,112 +1,32 @@
 <?php
 namespace app\index\model;
-use think\Model;//引入model类
+
+use think\Model;
 use think\Db;
-
-class User
-{
-	public function getUserInfo()
+	
+	class User 
 	{
-		// $data = Db::table('php_user')
-		// 			->select();
-		//name不用带表的前缀
-		// $data = Db::name('user')
-		// 		->where('uid','>','1')
-		// 		->select();
-		// //where的用法
-		// $data = Db::name('user')
-		// 		->where('uid > 2')
-		// 		->select();	
-		//where的第三种用法	
-		$con['uid'] = ['>', 1]; //等价语法uid > 1
-		$con['type'] = 1;
-		$data = Db::name('user')
-				->where($con)
-				->buildSql();
-
-
-
-		// $data = Db::name('user')
-		// 		->where('uid > 2')
-		// 		// ->fetchSql(true)//查看sql语句
-		// 		->select();
-		//有两种方式查看sql语句：fetchSql和buildSql
-		// $data = Db::name('user')
-		// 		->where('uid > 2')
-		// 		->buildSql();
-		dump($data);
-		die;
-				// ->select();	
-		// return $data;
+		public function getUserInfo()
+		{
+			/*
+				*where()三种使用方式
+			*/
+				//1.where();三个参数:1 查询关键字，2 比较运算符，3 值，第二个参数不传，默认是判等
+			//$data = Db::name('user')->field('uid,username')->where('uid','<',20)->select();
+				//2.where('字符串')
+			//$data = Db::name('user')->where('uid < 20')->select();
+				//3.数组形式
+			// $con['uid'] = ['<',20];
+			// $con['type'] = 0;
+			// $data = Db::name('user')->where($con)->select();
+			$data = Db::getTableInfo('user');
+			return $data;
+		}
+		public function test()
+		{
+			 $data = db('user')->field('uid,username')->select();
+			 return $data;
+			/*db('user')->insert('username'=>'tom','pwd');*/
+		}
+		
 	}
-
-
-	function multiTable()
-	{
-		//1 多表链接 where中需要时用字符串方式写链接条件
-		// $data = Db::table('php_blog b ,php_category c')
-		// 		->where("b.cid = c.cid")
-		// 		->select();
-		//2 使用join进行多表链接
-		$data = Db::table('php_blog')
-				->alias('b') //命名别名
-				->join('php_category c','b.cid=c.cid')
-				->select();
-
-		return $data;
-	}
-
-	public function find()
-	{
-
-		// return Db::name('user')->where('uid < 0')->select();
-		return Db::name('user')->where('uid < 0')->find();
-
-	}
-
-	public function multiCondition()
-	{
-		// return Db::name('user')
-		// 		->where('name' ,'like','%天%')
-		// 		->whereor('uid','<' ,3)
-		// 		->select();
-		return Db::name('user')
-				->where("name like '%天%' or uid <3")		
-				->select();
-	}
-
-	function tableInfo()
-	{
-		return Db::getTableInfo('php_user');
-	}
-
-	function time1()
-	{
-		//3使用英文单词表示
-		// return Db::name('blog')->whereTime('create_time', 'last week')
-		// 					->buildSql();
-
-		//原生字时间比较，
-		// return Db::name('blog')
-		//         ->where('create_time','<' ,'2017-07-05 0:0:0')
-			    // ->buildSql();
-			    // 
-		//使用whereTime
-	    return Db::name('blog')
-		        ->whereTime('create_time','<' ,'2017-07-05 0:0:0')
-			    ->buildSql();
-			   
-	}
-
-	//聚合查询
-	function countUser()
-	{
-		//助手函数表名不带前缀
-		// return db('user')->count("case when type = 0 then 1 end");
-		return db('user')->field("count(distinct type)")->where("type=0")->buildSql();
-
-		//去重统计 count\avg\sum
-		//SELECT  count(DISTINCT type)    FROM php_user
-	}
-}
-
