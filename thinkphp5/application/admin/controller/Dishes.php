cb06b101b091d08f984d2b1d13ec147b4a8bbb76
yxx_ql <1806771415@qq.com>
2017-07-21 19:30:21 +0800

    后台

--- thinkphp5/application/admin/controller/Dishes.php
+++ thinkphp5/application/admin/controller/Dishes.php
@@ -15,16 +15,109 @@ class Dishes extends Base
 			$this->assign('res', $res);
 			return $this->fetch();
 		}
+	//赋给添加页面
+	 public function add() {
+        
+
+        return $this->fetch();
+    }
 
 		//添加商品
 		public function addDishes()
 		{
-			return $this->fetch();
+			//接受提交过来的数据
+			$data = [
+
+					'menu_name' => input('menu_name'),
+					'menu_type' => input('menu_type'),
+					'menu_info' => input('menu_info'),
+					'menu_price'=> input('menu_price'),
+					'menu_icon' => input('menu_icon'),
+					'create_time'=> date('Y-m-d H:i:s',time()),
+			];
+	
+			//实例化模型
+			$mlModel = new DishesModel();
+			$res = $mlModel->add($data);
+
+			if($res>0)
+			{
+				$this->success('添加成功', 'getAllDishesList');
+			}else{
+
+				$this->error('添加失败......', 'add');
+			}
+ 
+			//return $this->fetch();
+		}
+
+		
+
+		//赋给编辑页面
+		public function edit()
+		{
+			//接受传过来的商品id
+			$id = $_GET['id'];
+
+			$mlM = Db('menu_list');
+			//mlM_info
+			$mlM_info = $mlM->where("menu_id = {$id}")->find();
+			
+			$this->assign('mlM_info', $mlM_info);
+			return $this->fetch('edit');
 		}
 
 		//编辑商品
-		public function updateDishes()
+		public function update()
 		{
-			return $this->fetch();
+			//接受提交过来的商品id
+			$id = $_POST['id'];
+
+			//接受提交过来的数据
+			$data = [
+
+					'menu_name' => input('menu_name'),
+					'menu_type' => input('menu_type'),
+					'menu_info' => input('menu_info'),
+					'menu_price'=> input('menu_price'),
+					'menu_icon' => input('menu_icon'),
+					'create_time'=> date('Y-m-d H:i:s',time()),
+			];
+
+
+
+			//实例化模型
+			$mlModel = new DishesModel();
+            $res = $mlModel->updateModel($id,$data); 
+			
+			if($res === false)
+			{
+					$this->error('编辑失败...','edit');
+			}else{
+				$this->success('编辑成功','getAllDishesList');
+			}
 		}
+
+
+			//删除商品
+		public function delete()
+		{
+			$id = $_GET['id'];
+			dump($id); exit;
+			$mlM = Db('menu_list');
+			$check = $mlM->where("menu_id={$id}")->find();
+			if($check == false)
+			{
+				$this->error('非法操作','getAllDishesList');
+			}
+
+			$back = $mlM->where("menu_id = {$id}")->delete();
+			if($back == true)
+			{
+				$this->success('删除成功','getAllDishesList');
+			}
+
+		}
+
+
 }
--- thinkphp5/application/admin/model/Dishes.php
+++ thinkphp5/application/admin/model/Dishes.php
@@ -13,4 +13,48 @@ class Dishes extends Model
 
 		return $result;
 	}
+
+	//添加商品
+	public function add($data)
+	{
+		//实例化对象
+		$mlM = Db('menu_list');
+		$result = $mlM->insert($data);
+
+		return $result;
+	}
+
+	//编辑商品方法
+	public function updateModel($id, $data)
+	{
+
+		//实例化对象
+		$mlM = Db('menu_list');
+		$result = $mlM->where("menu_id = {$id}")->update($data);
+		
+		return $result;
+	}
+
+	public function delete()
+	{
+	  
+		$id = $_POST['id'];
+		dump($id); exit;
+		//实例化对象
+		$mlM = Db('menu_list');
+		$check = $mlM->where("menu_id={$id}")->find();
+		if($check == false)
+		{
+			$this->error('非法操作', 'getAllDishesList');
+		}
+
+		$back = $mlM->where("menu_id={$id}")->delete();
+		if($back == true)
+		{
+			$this->success('删除成功', 'getAllDishesList');
+		}
+
+	}
+
+	
 }
