33d6803ca136445572343074a6c5103cf0b6fe65
yxx_ql <1806771415@qq.com>
2017-07-26 11:33:15 +0800

    修改我的订单

--- thinkphp5/application/index/model/Order.php
+++ thinkphp5/application/index/model/Order.php
@@ -9,28 +9,20 @@ Class  Order  extends  Model
 	public function userOrder($user_id)
 	{
 		
-		$result = Db('online_user,online_order')
-				 ->where("online_user.user_id =online_order.user_id")
+		$result = Db('online_user,order_status')
+				 ->where("online_user.user_id =order_status.user_id")
 				 ->select();
+
 		
 				return $result;
 	}
 	
 	
-	//用户订单列表（查询用户订单状态）
-	public function userOrderStatus($order_num)
-	{
-		
-		$result = Db('online_order,order_status')
-					->where("online_order.order_num=order_status.order_num")
-					->select();
-					
-					return $result;
-	}
+	
 	public function delUserOrder()
 	{
-		return Db::name('online_order')
-					// ->where('order_id',input('order_id'))
+		return Db::name('order_status')
+					// ->where('id',input('order_id'))
 					->where('order_num',input('order_num'))
 					->where('user_id',session('user_id'))
 					->delete();
