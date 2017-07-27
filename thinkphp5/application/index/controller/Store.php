c3a279e93457c242c1373a5034a1150348ea16c4
yxx_ql <1806771415@qq.com>
2017-07-27 17:59:10 +0800

    我的收藏

--- thinkphp5/application/index/controller/Store.php
+++ thinkphp5/application/index/controller/Store.php
@@ -9,6 +9,7 @@ use app\index\model\Online_shop;
 class  Store extends Controller
 {
 	protected $shop;
+
 	public function _initialize()
 	{
 		$this->shop = new Online_shop();
@@ -19,12 +20,50 @@ class  Store extends Controller
 		$shop_id = $_GET['shop_id'];
 			//获取商铺详情
 		$shop_info = $this->shop->select_shop_detail($shop_id);
+
 		$shop_detail = $shop_info;
 		$shop = $shop_info[0];//获取商店信息
 	
 		$this->assign('shop_info',$shop_info);
 		$this->assign('shop_detail',$shop_detail);
 		$this->assign('shop',$shop);
+		$this->assign('shop_id',$shop_id);
 		return $this->fetch();
 	}
+	
+	
+	//用户收藏店铺
+	public function UserFavorShop()
+	{
+		//接受商家shop_id
+		$shop_id = $_GET['shop_id'];
+		
+		$result = $this->shop->UserFavorShop($shop_id);
+		if($result == true)
+		{
+			$this->success('收藏成功','/index/userfavorites/user_favorites');
+		}else{
+			$this->error('收藏失败','storeDetail');
+		}
+	}
+	
+	
+	//用户取消店铺收藏
+	public function delFavor()
+	{
+		$shop_id = $_GET['shop_id'];
+		$result = $this->shop->delFavorModel($shop_id);
+		if($result == true)
+		{
+			$this->success('取消成功','/index/userfavorites/user_favorites');
+		}else{
+			$this->success('取消失败','/index/userfavorites/user_favorites');
+		}
+	}
+	
+	
+	
+	
+	
+	
 }
--- thinkphp5/application/index/model/Userfavorites.php
+++ thinkphp5/application/index/model/Userfavorites.php
@@ -8,9 +8,9 @@ Class  Userfavorites  extends  Model
 	//我的用户收藏
 	public function user_favorites()
 	{
-		$result = Db('user_favor fa,online_user us,menu_list me, online_shop sh')
+		$result = Db('user_favor fa,online_user us,online_shop sh')
 				
-				 ->where("fa.user_id =us.user_id and fa.menu_id = me.menu_id and fa.shop_id = sh.shop_id")
+				 ->where("fa.user_id =us.user_id and fa.shop_id = sh.shop_id")
 				 ->select();
 
 				return $result;
@@ -18,10 +18,10 @@ Class  Userfavorites  extends  Model
 	
 	
 			//删除我的收藏
-		public function delUserFavor()
+		public function delUserFavor($shop_id)
 		{
 			return Db::name('user_favor')
-				->where('menu_id',input('menu_id'))
+
 				->where('shop_id',input('shop_id'))
 				->where('user_id',session('user_id'))
 				->delete();
