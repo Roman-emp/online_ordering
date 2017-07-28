07616d3c3322aeea1973414e0f5eb7034afdcbd7
yxx_ql <1806771415@qq.com>
2017-07-28 10:32:20 +0800

    系统文章管理

---
+++ thinkphp5/application/admin/controller/Webmanager.php
@@ -0,0 +1,15 @@
+<?php
+namespace app\admin\controller;
+use app\admin\controller\Base;
+use app\admin\model\Dishes as DishesModel;
+
+class Webmanager extends Base
+{
+	
+	//站点信息
+	public function index()
+	{
+		
+	}
+	
+}
